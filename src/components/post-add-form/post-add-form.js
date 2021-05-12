783b3dab2aa6dca2744a40b76e9509e81e042da6
Den9 <den9.rcq@gmail.com>
2021-05-12 14:24:04 +0300

    sending new post data

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PostAddForm = ({ onAdd }) => {
-    return (
-        <form className="bottom-panel d-flex">
-            <input
-                type="text"
-                placeholder="О чем вы думаете сейчас?"
-                className="form-control new-post-label"
-            />
-            <button
-                type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => onAdd('Hello')}>
-                Добавить</button>
-        </form>
-    )
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        }
+
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    // Отслеживаем что пользователь ввёл в input
+    onValueChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    // Отправляем данные в body нового поста
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text)
+    }
+
+    render() {
+        return (
+            <form
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    placeholder="О чем вы думаете сейчас?"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-outline-secondary">
+                    Добавить</button>
+            </form>
+        )
+    }
 }
-export default PostAddForm;
